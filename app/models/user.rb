ad30f769a57939d664a4ffa77759dfb7c180320d
Janice Chan <janice.chan1288@gmail.com>
2022-09-20 14:32:47 -0400

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :items
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+
     has_secure_password
 end
