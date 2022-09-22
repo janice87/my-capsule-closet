7ecb952a38c427d7870845223bc4d1b19aa91ff8
Janice Chan <janice.chan1288@gmail.com>
2022-09-21 22:24:43 -0400

    fix so only signed in user can see

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -21,19 +21,13 @@ const Navbar = ({updateCurrentUser}) => {
         <Toolbar disableGutters>
         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
         {currentUser ? <Button color="secondary">Welcome {currentUser.name}!</Button> : null}  
-
         <Button color="secondary" to="/" component={Link}>HOME</Button> 
-        <Button color="secondary" to="/items" component={Link}>MY CLOSET</Button> 
-        <Button color="secondary" to="/items/new" component={Link}>ADD TO CLOSET</Button> 
-        <Button color="secondary" to="/capsules" component={Link}>MY CAPSULES</Button> 
+        {currentUser ? <Button color="secondary" to="/items" component={Link}>MY CLOSET</Button> : null}
+        {currentUser ? <Button color="secondary" to="/items/new" component={Link}>ADD TO CLOSET</Button> : null}
+        {currentUser ? <Button color="secondary" to="/capsules" component={Link}>MY CAPSULES</Button> : null}
         {currentUser ? <Button color="secondary" to="/logout" component={Link} onClick={handleLogout}>LOGOUT</Button>: <Button color="secondary" to="/login" component={Link}>LOGIN</Button>}
         {/* <Button color="secondary" to="/capsules/new" component={Link}>BUILD CAPSULES</Button>  */}
-            {/* {currentUser ? <Button color="secondary">Welcome {currentUser.name}!</Button> : null}            
-            <Button color="secondary" to="/" component={Link}>HOME</Button> 
-            <Button color="secondary" to="/rentals" component={Link}>RENTALS</Button>           
-            {currentUser ? <Button color="secondary" to={'/rentals/new'} component={Link}>ADD RENTALS</Button> : null}
-            {currentUser ? <Button color="secondary" to={`/users/${currentUser.id}`} component={Link}>MY RENTALS</Button> : null}                    
-            {currentUser ? <Button color="secondary" to="/logout" onClick={handleLogout}>LOGOUT</Button> : <Button color="secondary" to="/login" component={Link}>LOGIN </Button>}  */}
+          
         </Box>    
        </Toolbar>
       </Container>
