a4d79cae28574c721327514a82e0e06c3ba2393e
Janice Chan <janice.chan1288@gmail.com>
2022-09-21 20:50:16 -0400

    add edit item form

---
+++ client/src/components/ItemEditForm.js
@@ -0,0 +1,164 @@
+import React, { useContext, useState, useEffect } from "react"; 
+import {UserContext} from '../context/user' 
+import {useParams, useHistory} from 'react-router-dom'
+import { Box, Container, Button, Typography, TextField, Select, MenuItem } from '@mui/material';
+
+const ItemEditForm = ({onEditItem}) => {
+    const {currentUser} = useContext(UserContext);
+    const [updatedItem, setUpdatedItem] = useState({
+        item_name: "",
+        brand: "",
+        price: "",
+        image: "",
+        category: "",
+        user_id: currentUser.id
+    })
+    const [errors, setErrors] = useState([])
+    const {id} = useParams()
+    const history = useHistory()
+
+    useEffect(() => {
+        fetch(`/items/${id}`)
+        .then(res => {
+            if(res.ok) {
+                res.json().then(itemObj => setUpdatedItem(itemObj))
+            } else {
+                res.json().then(data => setErrors(data.errors))
+            }
+        })
+    }, [id])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch(`/items/${id}`, {
+            method: 'PATCH', 
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify(updatedItem)
+        })
+        .then(res => {
+            if(res.ok) {
+                res.json().then(data => onEditItem(data))
+                history.push(`/items`)
+            } else {
+                res.json().then(data => setErrors(data.errors))
+            }
+        })
+    }
+
+    const handleOnchange = (e) => {
+        setUpdatedItem({
+            ...updatedItem,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <div>
+        <Container maxWidth="xs">
+        <Box
+        m={1}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        >
+          <Typography variant="h5" align="center" style={{ marginBottom: "1em", marginTop: "1em" }}>Edit item in your closet</Typography> 
+          <br /> 
+        </Box>
+        </Container>  
+
+        <Container maxWidth="m">
+          <Box       
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          >          
+            {/* <FormControl onSubmit={handleSubmit} sx={{ m: 1, minWidth: 120 }}> */}
+            <form onSubmit={handleSubmit}>
+            {/* <form onSubmit={handleSubmit} sx={{ m: 1, minWidth: 120 }}> */}
+            <TextField                   
+                  id="outlined-size-small"
+                  name="item_name" 
+                  onChange={handleOnchange} 
+                  value={updatedItem.item_name}             
+                  style={{ marginBottom: "15px", width: "300px" }}  
+                  variant="outlined"
+                  label="Item Name"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}       
+                 /> 
+            <br/> 
+            <TextField 
+                  type="brand" 
+                  id="outlined-size-small"
+                  name="brand" 
+                  onChange={handleOnchange} 
+                  value={updatedItem.brand}             
+                  style={{ marginBottom: "15px", width: "300px" }}  
+                  variant="outlined"
+                  label="Brand"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}       
+                  /> 
+            <br/>            
+            <TextField                   
+                  id="outlined-size-small"
+                  name="price" 
+                  onChange={handleOnchange} 
+                  value={updatedItem.price}             
+                  style={{ marginBottom: "15px", width: "300px" }}  
+                  variant="outlined"
+                  label="Price"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}       
+                  /> 
+            <br/>     
+            <TextField                   
+                  id="outlined-size-small"
+                  name="image" 
+                  onChange={handleOnchange} 
+                  value={updatedItem.image}             
+                  style={{ marginBottom: "15px", width: "300px" }}  
+                  variant="outlined"
+                  label="Image URL"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}       
+                  /> 
+            <br/>          
+            
+        <Select
+          name="category"
+          id="category"
+          value={updatedItem.category}
+          label="Category"              
+        onChange={handleOnchange}    
+        >    
+          <MenuItem value="Blouses & Tops">BLOUSES & TOPS</MenuItem>
+          <MenuItem value="Bottoms">BOTTOMS</MenuItem>
+          <MenuItem value="Coats">COATS</MenuItem>
+          <MenuItem value="Dresses">DRESSES</MenuItem>
+          <MenuItem value="Handbags">HANDBAGS</MenuItem>
+          <MenuItem value="Shoes">SHOES</MenuItem>         
+        </Select>  
+
+            <br/>
+            <Button type="submit" variant="outlined">Submit</Button>
+             {errors ? errors.map(error => <li key={error}>{error}</li>) : null }            
+            {/* </FormControl>   */}
+            </form>  
+           
+          </Box>
+        </Container>   
+
+        </div>
+    )
+
+}
+
+export default ItemEditForm
