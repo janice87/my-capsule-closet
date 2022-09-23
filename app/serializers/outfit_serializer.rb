afbd115a1e1c584cd8288006c80048c2127bdfb0
Janice Chan <janice.chan1288@gmail.com>
2022-09-22 22:34:52 -0400

    update serializer

--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -1,4 +1,8 @@
 class OutfitSerializer < ActiveModel::Serializer
-  attributes :id, :outfit_name, :capsule_id
+  attributes :id, :outfit_name, :capsule_id, :capsule
   has_many :items
+  
+  def capsule
+    self.object.capsule.capsule_name
+  end
 end
