9f9544b3ea2ca4d830df3ff3841e54f78ae9eda2
Janice Chan <janice.chan1288@gmail.com>
2022-09-28 13:48:37 -0400

    add outfit item to serializer

--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -1,6 +1,7 @@
 class OutfitSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :outfit_name, :capsule_id, :capsule
   has_many :items
+  has_many :outfit_items
   belongs_to :user
   
   def capsule
