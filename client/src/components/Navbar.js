6842298aae2d9117b5d317e20f8b748caf226580
Janice Chan <janice.chan1288@gmail.com>
2022-09-28 13:49:24 -0400

    update navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = ({updateCurrentUser}) => {
             method: 'DELETE'
         })
         updateCurrentUser(false)
-        history.push('/login')
+        history.push('/home')
     }
 
     return (
@@ -20,14 +20,14 @@ const Navbar = ({updateCurrentUser}) => {
       <Container maxWidth="md">
         <Toolbar disableGutters>
         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
         {currentUser ? <Button color="secondary">Welcome {currentUser.name}!</Button> : null}  
         <Button color="secondary" to="/" component={Link}>HOME</Button> 
         {currentUser ? <Button color="secondary" to="/items" component={Link}>MY CLOSET</Button> : null}
         {currentUser ? <Button color="secondary" to="/items/new" component={Link}>ADD TO CLOSET</Button> : null}
         {currentUser ? <Button color="secondary" to="/capsules" component={Link}>MY CAPSULES</Button> : null}
-        {currentUser ? <Button color="secondary" to="/logout" component={Link} onClick={handleLogout}>LOGOUT</Button>: <Button color="secondary" to="/login" component={Link}>LOGIN</Button>}
-        {/* <Button color="secondary" to="/capsules/new" component={Link}>BUILD CAPSULES</Button>  */}
-          
+        {currentUser ? <Button color="secondary" to="/home" component={Link} onClick={handleLogout}>LOGOUT</Button>: <Button color="secondary" to="/login" component={Link}>LOGIN</Button>}
+                
         </Box>    
        </Toolbar>
       </Container>
