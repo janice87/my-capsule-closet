dde5f32f7658e87e41672fdef16a4ad84a948bab
Janice Chan <janice.chan1288@gmail.com>
2022-10-04 20:00:14 -0400

    update outfit container

--- client/src/components/ItemContainer.js
+++ client/src/components/ItemContainer.js
@@ -1,24 +1,13 @@
 import {useState} from 'react'
 import { Box, Typography} from '@mui/material';
 import ItemList from "./ItemList";
-// import BuildOutfits from "./BuildOutfits";
-// import NewOutfitForm from './NewOutfitForm';
 import Search from './Search'
 
-// const ItemContainer = ({items, capsules, outfits, updateItemObj, onAddOutfitItem, onAddNewOutfit}) => {
-  const ItemContainer = ({items, updateItemObj,}) => {
-  // const [showForm, setShowForm] = useState(false)
-  // const [showOutfitForm, setShowOutfitForm] = useState(false)
+
+const ItemContainer = ({items, updateItemObj,}) => {
+
   const [searchTerm, setSearchTerm] = useState("")
   const [filterBy, setFilterBy] = useState("All")
-
-  // const handleShowForm = () => {
-  //   setShowForm((showForm) => !showForm)
-  // }
-
-  // const handleShowOutfitForm = () => {
-  //   setShowOutfitForm((showOutfitForm) => !showOutfitForm)
-  // }
 
   const handleSearchTerm = (search) => {
     setSearchTerm(search)
@@ -46,14 +35,7 @@ import Search from './Search'
       display="wrap"        
       style={{ marginBottom: "2em", marginTop: "1em" }}> 
           <Typography variant="h4" align="center" style={{ marginBottom: ".3em", marginTop: ".3em" }}> MY CLOSET </Typography>  
-          <Search searchTerm={searchTerm} onHandleSearchTerm={handleSearchTerm} filterBy={filterBy} onHandleFilter={onHandleFilter} />
-
-          {/* <Box m={1} display="flex" justifyContent="center" alignItems="center">
-           <Button size="medium" variant="contained" onClick={handleShowOutfitForm} color="primary" style={{ marginRight: ".5em", marginLeft: ".5em" }}>ADD OUTFIT</Button>   
-           <Button size="medium" variant="contained" onClick={handleShowForm} color="primary" style={{ marginRight: ".5em", marginLeft: ".5em" }}>BUILD OUTFIT</Button>            
-          </Box>
-           {showOutfitForm ? <NewOutfitForm onAddNewOutfit={onAddNewOutfit} capsules={capsules} /> : null} 
-           {showForm ? <BuildOutfits onAddOutfitItem={onAddOutfitItem} outfits={outfits} items={items} /> : null}     */}
+          <Search searchTerm={searchTerm} onHandleSearchTerm={handleSearchTerm} filterBy={filterBy} onHandleFilter={onHandleFilter} />      
           <ItemList items={filteredItems} updateItemObj={updateItemObj} /> 
           </Box> 
       </div>
